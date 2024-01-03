4b122652a6c749a0929b26885e97ac9ccde124b6
Vuong Tran <121814098+Vtrandev@users.noreply.github.com>
2024-01-02 22:19:45 -0800

    Limit input to display box

--- calculator.js
+++ calculator.js
@@ -3,50 +3,51 @@ const display = document.querySelector(".display");
 const btn = document.querySelectorAll(".btn");
 
 // Keyboard inputs
-addEventListener('keypress', (e) => inputEventFunction(e))
+addEventListener("keypress", (e) => inputEventFunction(e));
 
 // Button click inputs
-btn.forEach((btn) => {
-  btn.addEventListener("click", inputEventFunction);
-});
+btn.forEach((btn) => btn.addEventListener("click", inputEventFunction));
 
 // Callback function
 function inputEventFunction(e) {
-    const input = e.key || e.target.innerText;
+  const input = e.key || e.target.innerText;
 
-    if (input === "=" || input === "Enter") {
-      display.innerText = parseFloat(operate(numObj).toFixed(9));
-      return clearDisplay();
-    }
+  // Calculate results
+  if (input === "=" || input === "Enter") {
+    display.innerText = parseFloat(operate(numObj).toFixed(9));
+    return clearDisplay();
+  }
 
-    if (numObj["operator"] && ["+", "-", "/", "*"].includes(input)) {
-      numObj["first"] = operate(numObj);
-      numObj["operator"] = input;
-      delete numObj["second"];
-      return display.innerText = numObj["first"];
-    }
+  if (numObj["operator"] && ["+", "-", "/", "*"].includes(input)) {
+    numObj["first"] = operate(numObj);
+    numObj["operator"] = input;
+    delete numObj["second"];
+    return (display.innerText = numObj["first"]);
+  }
 
-    if (input === "AC") {
-      display.innerText = 0;
-      return clearDisplay();
-    }
+  if (input === "AC") {
+    display.innerText = 0;
+    return clearDisplay();
+  }
 
-    // Add numbers and operator to the object
-    if (numObj["operator"]) {
-      addNumToObject(numObj, "second", input);
-      display.innerText = parseFloat(numObj["second"]);
-    } else if (["+", "-", "/", "*"].includes(input)) {
-      numObj["operator"] = input;
-    } else {
-      addNumToObject(numObj, "first", input);
-      display.innerText = parseFloat(numObj["first"]);
-    }
-
-    console.log(numObj);
+  // Add numbers and operator to the object
+  if (numObj["operator"]) {
+    addNumToObject(numObj, "second", input);
+    display.innerText = parseFloat(numObj["second"]);
+  } else if (["+", "-", "/", "*"].includes(input)) {
+    numObj["operator"] = input;
+  } else {
+    addNumToObject(numObj, "first", input);
+    display.innerText = parseFloat(numObj["first"]);
+  }
 }
 
 function addNumToObject(obj, string, input) {
+  // Limit input to 11 digits
+  if (obj[string] && obj[string].length > 11) return;
+
   if (!obj[string]) obj[string] = 0;
+
   obj[string] += input;
   return obj;
 }
